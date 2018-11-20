fea2bd8327f06ba4297a2fb89a134168883a6cd1
Piero Toffanin <pt@masseranolabs.com>
2018-11-20 15:04:28 -0500

    Licenses, README changes

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 # nodeodm-proxy
 A reverse proxy, load balancer and task tracker for NodeODM
 
-## Documentation
+## Installation
 
-Coming soon
+```bash
+npm install
+```
+
+## Usage
+
+To start the program:
+
+```bash
+node index.js
+```
+
+To connect to the CLI and connect new NodeODM instances:
+
+```bash
+telnet localhost 3000
+> HELP
+> NODES ADD nodeodm-host 3001
+> NODES LIST
+```
+
+Use a web browser to connect to `http://localhost:3000`.
+
+See `node index.js --help` for more configuration options.
+
+## Disclaimer
+
+This software is alpha. More documentation is coming soon.
--- libs/nodes.js
+++ libs/nodes.js
@@ -1,3 +1,20 @@
+/**
+ *  nodeodm-proxy - A reverse proxy, load balancer and task tracker for NodeODM
+ *  Copyright (C) 2018-present MasseranoLabs LLC
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 const Node = require('./classes/Node');
 const fs = require('fs');
 const logger = require('./logger');
@@ -73,7 +90,7 @@ module.exports = {
 
     findBestAvailableNode: async function(numImages, update = false){
         if (update) await this.updateInfo();
-        
+
         const candidates = nodes.filter(n => n.isOnline() && 
                                                    (n.getInfo().maxImages < 0 || n.getInfo().maxImages >= numImages));
         if (!candidates.length) return null;
