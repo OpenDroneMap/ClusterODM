a9a26ea6fd3e6571163361078a563a38c8bb2f66
Piero Toffanin <pt@masseranolabs.com>
2019-10-14 18:41:20 -0400

    Properly read SSH key IDs, snapshot IDs

--- libs/asr-providers/hetzner.js
+++ libs/asr-providers/hetzner.js
@@ -19,6 +19,7 @@ const AbstractASRProvider = require('../classes/AbstractASRProvider');
 const netutils = require('../netutils');
 const S3 = require('../S3');
 const logger = require('../logger');
+const axios = require('axios');
 const fs = require('fs');
 
 module.exports = class HetznerAsrProvider extends AbstractASRProvider{
@@ -81,6 +82,8 @@ module.exports = class HetznerAsrProvider extends AbstractASRProvider{
             const exists = await new Promise((resolve) => fs.exists(this.getConfig("sshKey.path"), resolve));
             if (!exists) throw new Error("Invalid config key sshKey.path: file does not exist");
         }
+
+        this.fpCache = {};
     }
 
     getDriverName(){
@@ -104,6 +107,57 @@ module.exports = class HetznerAsrProvider extends AbstractASRProvider{
 
         return this.getImageSlugFor(imagesCount) !== null && 
                (minImages === -1 || imagesCount >= minImages);
+    }
+
+    async getSSHKeyId(){
+        const fingerprint = this.getConfig("sshKey.fingerprint");
+        if (!fingerprint) throw new Error("Need to specify SSH Key fingerprint in configuration.");
+        if (this.fpCache[fingerprint]) return this.fpCache[fingerprint];
+
+        // We need to fetch the ssh key ID
+        const response = await axios.get(`https://api.hetzner.cloud/v1/ssh_keys?page=1&per_page=9999999&fingerprint=${encodeURIComponent(fingerprint)}`, { 
+            timeout: 10000,
+            headers: {
+                Authorization: `Bearer ${this.getConfig("apiToken")}`
+            }
+        });
+
+        if (response.status === 200){
+            const { ssh_keys } = response.data;
+            const key = ssh_keys.find(sk => sk.fingerprint === fingerprint);
+            if (key && key.id){
+                this.fpCache[fingerprint] = key.id;
+                return key.id;
+            }else{
+                throw new Error(`SSH Key with fingerprint ${fingerprint} not found.`);
+            }
+        }else{
+            throw new Error(`Cannot contact Hetzner API: ${response.status}`);
+        }
+    }
+
+    async getSnapshotId(){
+        let imageName = this.getConfig("image");
+
+        // We need to fetch the imageID
+        const response = await axios.get("https://api.hetzner.cloud/v1/images?type=snapshot&description=nodeodm-image&page=1&per_page=9999999", { 
+            timeout: 10000,
+            headers: {
+                Authorization: `Bearer ${this.getConfig("apiToken")}`
+            }
+        });
+
+        if (response.status === 200){
+            const { images } = response.data;
+            const img = images.find(img => img.description === imageName);
+            if (img && img.id){
+                return img.id;
+            }else{
+                throw new Error(`Snapshot ${imageName} not found.`);
+            }
+        }else{
+            throw new Error(`Cannot contact Hetzner API: ${response.status}`);
+        }
     }
 
     async setupMachine(req, token, dm, nodeToken){
@@ -163,16 +217,17 @@ module.exports = class HetznerAsrProvider extends AbstractASRProvider{
         ];
 
         if (this.getConfig("snapshot")){
-            args.push("--hetzner-image");
-        }else{
             args.push("--hetzner-image-id");
+            args.push(await this.getSnapshotId());
+        }else{
+            args.push("--hetzner-image");
+            args.push(this.getConfig("image"));
         }
-        args.push(this.getConfig("image"));
 
 
         if (this.getConfig("sshKey.fingerprint", "")){
             args.push("--hetzner-existing-key-id");
-            args.push(this.getConfig("sshKey.fingerprint"));
+            args.push(await this.getSSHKeyId());
         }
 
         if (this.getConfig("sshKey.path", "")){
