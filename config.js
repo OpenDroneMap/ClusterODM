560dc137304098ae0f53c0ec4f32a12cc56391f6
Stephen Mather <smathermather@protonmail.com>
2020-03-26 13:03:34 +0000

    update configs to include token

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ try{
 
 let argDefs = {
     string: ['port', 'admin-cli-port', 'admin-pass', 'admin-web-port',
-            'cloud-provider', 'downloads-from-s3', 'log-level',
+            'cloud-provider', 'downloads-from-s3', 'token', 'log-level',
             'upload-max-speed', 'ssl-key', 'ssl-cert', 'secure-port',
             'public-address', 'config',
             'asr'],
@@ -66,6 +66,7 @@ Options:
     --downloads-from-s3 <URL>	Manually set the S3 URL prefix where to redirect /task/<uuid>/download requests. (default: do not use S3, forward download requests to nodes, unless the autoscaler is setup, in which case the autoscaler's S3 configuration is used) 
     --no-splitmerge	By default the program will set itself as being a cluster node for all split/merge tasks. Setting this option disables it. (default: false)
     --public-address <http(s)://host:port>	Should be set to a public URL that nodes can use to reach ClusterODM. (default: match the "host" header from client's HTTP request)
+    --token <token> Sets a token that needs to be passed for every request. This can be used to limit access to the node only to token holders. (default: none)
     --debug 	Disable caches and other settings to facilitate debug (default: false)
     --ssl-key <file>	Path to .pem SSL key file
     --ssl-cert <file>	Path to SSL .pem certificate
