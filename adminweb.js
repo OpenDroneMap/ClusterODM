b92527140b58bb9e0039819e7d01219b86ed2776
Piero Toffanin <pt@masseranolabs.com>
2021-02-26 09:39:34 -0500

    Minor bootstrap fixes
    Minor bootstrap fixes
    
    Minor bootstrap fixes

--- adminweb.js
+++ adminweb.js
@@ -45,8 +45,7 @@ module.exports = {
         <head>
           <meta charset="UTF-8">
           <title>ClusterODM</title>
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+          <link href="/bootstrap.min.css" rel="stylesheet">
         </head>
         <body>`;
 
@@ -57,7 +56,7 @@ module.exports = {
         app.get('/', (req, res) => {
             res.send(`${htmlHead}
               <div class="container">
-                <h1>ClusterODM ${package_info.version}</h1>
+                <h1 class="text-center mt-2 mb-2">ClusterODM ${package_info.version}</h1>
                 <table class="table table-hover table-striped">
                   <thead>
                     <tr class="text-white bg-primary">
@@ -79,7 +78,7 @@ module.exports = {
                       return `<tr>
                       <td>${idx + 1}</td>
                       <td>${node}</td>
-                      <td>${node.isOnline() ? '<span class="badge bg-success">Online</span>' : '<span class="badge badge-danger">Offline</span>'}</td>
+                      <td>${node.isOnline() ? '<span class="badge bg-success">Online</span>' : '<span class="badge bg-danger">Offline</span>'}</td>
                       <td>${node.getTaskQueueCount()}/${node.getMaxParallelTasks()}</td>
                       <td>${node.getEngineInfo()}</td>
                       <td>${node.getVersion()}</td>
