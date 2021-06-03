75bda3dd4f4f8cc56fe06342435eb38dcad498c1
Piero Toffanin <pt@masseranolabs.com>
2021-06-03 15:00:20 -0400

    Native Windows Support (#82)
    * Winbundle support
    
    * Fix config-default, paths
    
    * Add libcurl
    
    * Include libs contents

---
+++ winbundle.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+const spawnSync = require('child_process').spawnSync;
+const path = require('path');
+const async = require('async');
+const archiver = require('archiver');
+
+const bundleName = "clusterodm-windows-x64.zip";
+
+async.series([
+    cb => {
+        // Cleanup directories
+        console.log("Cleaning up folders");
+        for (let dir of ["data", "tmp"]){
+            for (let entry of fs.readdirSync(dir)){
+                if (entry !== ".gitignore"){
+                    const e = path.join(dir, entry);
+                    console.log(`Removing ${e}`);
+                    if (fs.lstatSync(e).isDirectory()){
+                        fs.rmdirSync(e, { recursive: true });
+                    }else{
+                        fs.unlinkSync(e);
+                    }
+                }
+            }
+        }
+        cb();
+    },
+
+    cb => {
+        console.log("Building executable");
+        const code = spawnSync('nexe.cmd', ['index.js', '-t', 'windows-x64-12.16.3', '-o', 'clusterodm.exe', '-r', 'libs/**'], { stdio: "pipe"}).status;
+
+        if (code === 0) cb();
+        else cb(new Error(`nexe returned non-zero error code: ${code}`));
+    },
+    cb => {
+        // Zip
+        const outFile = path.join("dist", bundleName);
+        if (!fs.existsSync("dist")) fs.mkdirSync("dist");
+        if (fs.existsSync(outFile)) fs.unlinkSync(outFile);
+
+        let output = fs.createWriteStream(outFile);
+        let archive = archiver.create('zip', {
+            zlib: { level: 5 } // Sets the compression level (1 = best speed since most assets are already compressed)
+        });
+
+        archive.on('finish', () => {
+            console.log("Done!");
+            cb();
+        });
+
+        archive.on('error', err => {
+            console.error(`Could not archive .zip file: ${err.message}`);
+            cb(err);
+        });
+
+        const files = [
+            "data",
+            "docs",
+            "letsencrypt",
+            "public",
+            "tmp",
+            "config-default.json",
+            "LICENSE",
+            "package.json",
+            "clusterodm.exe"
+        ];
+
+        archive.pipe(output);
+        files.forEach(file => {
+            console.log(`Adding ${file}`);
+            let stat = fs.lstatSync(file);
+            if (stat.isFile()){
+                archive.file(file, {name: path.basename(file)});
+            }else if (stat.isDirectory()){
+                archive.directory(file, path.basename(file));
+            }else{
+                logger.error(`Could not add ${file}`);
+            }
+        });
+
+        // Plus node-libcurl
+        console.log("Adding node_modules/node-libcurl")
+        archive.directory(path.join("node_modules", "node-libcurl"), path.join("node_modules", "node-libcurl"));
+        
+        archive.finalize();
+    }
+], (err) => {
+    if (err) console.log(`Bundle failed: ${err}`);
+    else console.log(`Bundle ==> dist/${bundleName}`);
+});
+
+
